3e5c0a3f6bde504a13017e3edfe8d31c1e5a60eb
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2026-02-23 15:15:03 +0300

    Change docker file for upload image

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,17 +4,9 @@ from fastapi.staticfiles import StaticFiles
 from src.api.v1.endpoints.products_api import router as product_router
 
 
-tags_metadata = [
-    {
-        "name": "Продукты",
-        "description": "Управление товарами магазина: создание, удаление, обновление и просмотр.",
-    },
-]
-
 app = FastAPI(
     title="Goods Store TMA API",
     version="1.0.0",
-    openapi_tags=tags_metadata
     )
 
 # Создаем папку для загрузок, если её нет
@@ -26,7 +18,7 @@ if not os.path.exists(UPLOAD_DIR):
 # Теперь файл uploads/items/photo.jpg будет доступен по ссылке http://localhost:8000/static/items/photo.jpg
 app.mount("/static", StaticFiles(directory=UPLOAD_DIR), name="static")
 # Подключаем роутер с префиксом
-app.include_router(product_router, prefix="/api/v1/products")
+app.include_router(product_router, prefix="/api/v1/products", tags=["Продукты"])
 
 
 @app.get("/", tags=["Root"])
