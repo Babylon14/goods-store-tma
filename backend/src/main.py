ea570db72e85f1a743354118f6951c901875f610
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2026-02-23 14:29:12 +0300

    Added template for upload images

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,4 +1,6 @@
+import os
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from src.api.v1.endpoints.products_api import router as product_router
 
 
@@ -15,6 +17,14 @@ app = FastAPI(
     openapi_tags=tags_metadata
     )
 
+# Создаем папку для загрузок, если её нет
+UPLOAD_DIR = "uploads"
+if not os.path.exists(UPLOAD_DIR):
+    os.makedirs(UPLOAD_DIR)
+
+# Монтируем папку со статикой
+# Теперь файл uploads/items/photo.jpg будет доступен по ссылке http://localhost:8000/static/items/photo.jpg
+app.mount("/static", StaticFiles(directory=UPLOAD_DIR), name="static")
 # Подключаем роутер с префиксом
 app.include_router(product_router, prefix="/api/v1/products")
 
