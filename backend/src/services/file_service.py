f598625d936a5497a1a7ffca550cbb6add3db5f6
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2026-02-23 14:38:26 +0300

    Create service for work with files

---
+++ backend/src/services/file_service.py
@@ -0,0 +1,28 @@
+import os
+import uuid
+import shutil
+
+from fastapi import UploadFile
+
+
+UPLOAD_DIR = "uploads"
+
+async def save_upload_file(upload_file: UploadFile, folder: str = "products") -> str:
+    """Сохраняет файл и возвращает путь к нему"""
+
+    # 1. Создаем подпапку (например, uploads/products)
+    dest_folder = os.path.join(UPLOAD_DIR, folder)
+    if not os.path.exists(dest_folder):
+        os.makedirs(dest_folder)
+
+    # 2. Геенерируем уникальное имя: uuid + оригинальное расширение
+    file_extension = os.path.splitext(upload_file.filename)[1]
+    file_name = f"{uuid.uuid4()}{file_extension}"
+    file_path = os.path.join(dest_folder, file_name)
+
+    # 3. Сохраняем файл на диск
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(upload_file.file, buffer)
+
+    # 4. Возвращаем путь, который будет храниться в БД
+    return f"/static/{folder}/{file_name}"
