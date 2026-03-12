f073bbbcd13dd8bf1442edeb064272a62559ccea
Andrey Zablotskiy <zablotskyandrew@yandex.ru>
2026-03-12 13:18:24 +0300

    Update keyboards

--- backend/src/bot/keyboards/keyboadrs.py
+++ backend/src/bot/keyboards/keyboadrs.py
@@ -1,15 +1,17 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
 
 def get_main_menu(webapp_url: str):
     """Главное меню с кнопкой открытия Mini App"""
-    
-    keyboard = ReplyKeyboardMarkup(keyboard=[
-        # Эта кнопка откроет React-фронтенд внутри Telegram
-        [KeyboardButton(text="Открыть магазин 🛍️", web_app=WebAppInfo(url=webapp_url))],
-        [KeyboardButton(text="🚀 О нас"), KeyboardButton(text="❓ Помощь")]
-    ], resize_keyboard=True
-    )
-    return keyboard
+
+    builder = ReplyKeyboardBuilder()
+    builder.button(text="Открыть магазин 🛍️", web_app=WebAppInfo(url=webapp_url))
+    builder.button(text="📦 Мои заказы")
+    builder.button(text="🚀 О нас")
+    builder.button(text="❓ Помощь")
+    builder.adjust(1, 1, 2)
+    return builder.as_markup(resize_keyboard=True)
+
 
 
